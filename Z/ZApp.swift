c9230c6cd1bc16a8230b7dba0139bb4c71f158c7
Lasitha Weligampola <lasiprabo@gmail.com>
2024-01-27 09:55:33 -0500

    Initial Commit

---
+++ Z/ZApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZApp.swift
+//  Z
+//
+//  Created by Lasitha Weligampola on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct ZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
